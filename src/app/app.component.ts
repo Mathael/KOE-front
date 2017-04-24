faf41c7c63e0a364894d9220963e655943205f65
LEBOC Philippe <LebocPhilippe@live.be>
2017-04-24 10:01:36 +0200

    fix(class): add active class on click on navigation button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,5 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    ngOnInit(): void {
-    }
+    ngOnInit(): void {}
 }
